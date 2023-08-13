86330cb7bb607d8672b6a6ec2a32b6a7f9ce6812
Azizjon <aliqulovazizjon68@gmail.com>
2023-08-14 01:32:32 +0500

    updated

--- index.js
+++ index.js
@@ -7,16 +7,13 @@ const main = async () => {
 
   let university = data.universities;
 
-  for(let i = 0;i<university.length,i++)
-  {
-    let faculty = university[i].faculties
-    for(let j = 0;j<university[i].length;j++){
-    await  parsing(region,university[i].value,faculty[j].value)
-      console.log(`Bo'ldi : ${university[i].text}  ${faculty[j].text} `)
-
+  for (let i = 0; i < university.length; i++) {
+    let faculty = university[i].faculties;
+    for (let j = 0; j < university[i].length; j++) {
+      await parsing(region, university[i].value, faculty[j].value);
+      console.log(`Bo'ldi : ${university[i].text}  ${faculty[j].text} `);
     }
   }
 };
 
-
-main()
+main();
